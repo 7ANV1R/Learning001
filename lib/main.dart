8a0086aed70c9999a7614f7030e4983e6dc259b2
7ANV1R <ibnmizan7878@gmail.com>
2021-01-19 01:08:38 +0600

    putting in Stless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          title: Text('Appbar'),
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
           backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(child: Text('Hello')),
-      ),
-    ),
-  );
+        ));
+  }
 }
